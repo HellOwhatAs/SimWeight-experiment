200485e9e105bbad39929917b97c6d83868c1566
HellOwhatAs <xjq701229@outlook.com>
2024-01-21 14:19:36 +0800

    commit

--- draft.py
+++ draft.py
@@ -1,13 +1,14 @@
 from extract_data import Result
 import pickle
 import weight_experiment
+import time
 
 with open("./beijing.pkl", "rb") as f:
     tmp: Result = pickle.load(f)
     (nodes, edges, trips) = tmp
     
 g = weight_experiment.DiGraph(nodes.shape[0], [(i['u'], i['v']) for _, i in edges.iterrows()])
-
-res = g.experiment(edges["length"].to_list(), trips["test"])
-
-print(res / len(trips))
+t = time.time()
+res = g.experiment(edges["length"], trips["test"])
+print(time.time() - t)
+print(res / len(trips["test"]))
--- weight_experiment/weight_experiment.pyi
+++ weight_experiment/weight_experiment.pyi
@@ -2,7 +2,19 @@ from typing import List, Tuple
 
 class DiGraph:
     def __init__(self, n: int, edges: List[Tuple[int, int]]) -> None:
-        ...
+        self.n: int
+        "num of vertexs"
+        self.edges: List[Tuple[int, int]]
+        "`edges[edge_idx] = (u, v)`"
+        self.adjlist: List[List[Tuple[int, int]]]
+        "`adjlist[u] = [(v1, edge_idx), ..., (vn, edge_idx)]`"
 
     def experiment(self, weight: List[int], trips: List[List[int]]) -> int:
-        ...
+        """
+        Args:
+            weight: `weight[edge_idx]` is the weight of the `edge_idx` edge
+            trips: `[trip1, ..., tripn]` where `trip = [edge_idx, ...]`
+
+        Returns:
+            int: num of trips that is shortest path under weight
+        """
