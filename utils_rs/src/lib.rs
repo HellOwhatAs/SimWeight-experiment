9de603aafd1f6216cbb39910cf15788eee81891b
HellOwhatAs <xjq701229@outlook.com>
2024-01-23 19:32:53 +0800

    1. compare length ❎    compare List[edge_id]✅
    2. weight usize → f64

--- utils_rs/src/lib.rs
+++ utils_rs/src/lib.rs
@@ -1,6 +1,7 @@
 use pyo3::prelude::*;
 use rayon::prelude::*;
-use pathfinding::prelude::dijkstra;
+use pathfinding::directed::dijkstra::dijkstra_eid;
+use ordered_float::OrderedFloat;
 
 #[pyclass]
 struct DiGraph {
@@ -31,14 +32,13 @@ impl DiGraph {
     fn adjlist(&self) -> PyResult<Vec<Vec<(usize, usize)>>> {
         Ok(self.adjlist.clone())
     }
-    pub fn experiment(&self, weight: Vec<usize>, trips: Vec<Vec<usize>>) -> usize {
+    pub fn experiment(&self, weight: Vec<f64>, trips: Vec<Vec<usize>>) -> usize {
         let successors = |n: &usize| {
-            self.adjlist[*n].iter().map(|(t, edge_idx)| (*t, weight[*edge_idx]))
+            self.adjlist[*n].iter().map(|(t, edge_idx)| (*t, OrderedFloat(weight[*edge_idx]), *edge_idx))
         };
         trips.into_par_iter().map(|trip| {
-            let d = dijkstra(&self.edges[*trip.first().unwrap()].0, successors, |p| *p == self.edges[*trip.last().unwrap()].1).unwrap().1;
-            let l: usize = trip.into_iter().map(|edge_id| weight[edge_id]).sum();
-            (d == l) as usize
+            let (pred, _) = dijkstra_eid(&self.edges[*trip.first().unwrap()].0, successors, |p| *p == self.edges[*trip.last().unwrap()].1).unwrap();
+            (pred == trip) as usize
         }).sum()
     }
 }
