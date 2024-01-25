ef63259ac57e588455e54d541157499e06b987e6
HellOwhatAs <xjq701229@outlook.com>
2024-01-25 16:21:00 +0800

    basic `Rower`

--- rower_model.py
+++ rower_model.py
@@ -1,6 +1,9 @@
-from typing import Tuple, Union
+from typing import Tuple, Union, List, Callable, Dict
 from more_itertools import pairwise, flatten
+import utils_rs
+import pandas as pd
 import torch
+import numpy as np
 
 class WeightEmbedding(torch.nn.Module):
     
@@ -23,6 +26,19 @@ class WeightEmbedding(torch.nn.Module):
         if self.embedding is not None and field is not None:
             res += self.embedding(field)
         return res
+    
+class Rower(torch.nn.Module):
+
+    def __init__(self, edges: pd.DataFrame):
+        super().__init__()
+        self.edge_attrs = torch.nn.Parameter(torch.from_numpy(edges["length"].to_numpy(dtype=np.float32)).view(-1, 1), requires_grad=False)
+        self.edge_weight = WeightEmbedding(edges.shape[0], num_fields=(1, 16))
+
+    def forward(self, trips: List[torch.LongTensor]) -> torch.Tensor:
+        return torch.stack([self.edge_weight(trip.view(-1, 1), self.edge_attrs[trip]).sum() for trip in trips])
+    
+def bpr_loss_reverse(positive_lengths: torch.Tensor, negative_lengths: torch.Tensor) -> torch.Tensor:
+    return -torch.nn.functional.logsigmoid(negative_lengths.unsqueeze(1) - positive_lengths).sum()
 
 if __name__ == "__main__":
     from extract_data import Result
@@ -39,24 +55,19 @@ if __name__ == "__main__":
         tmp: Result = pickle.load(f)
         (nodes, edges, trips) = tmp
 
-    lossfunc = torch.nn.MSELoss()
-    m = WeightEmbedding(edges.shape[0], (1, 16)).to(device)
-    optimizer = torch.optim.Adam(m.parameters(), lr=0.01)
-
-    x_idx = torch.arange(0, edges.shape[0]).to(device).view(-1, 1)
-    x_train = torch.Tensor(edges["length"].to_numpy()).to(device).view(-1, 1)
-    y_train = torch.Tensor(edges["length"].to_numpy()).to(device).view(-1, 1)
-
-    m.train()
-    for _ in (pbar := tqdm(range(10000))):
-        result = m(x_idx[:1000], x_train[:1000])
-        loss = lossfunc(result, y_train[:1000])
-        pbar.set_postfix_str(f"loss={loss.item():.5f}")
+    model = Rower(edges).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    graph = utils_rs.DiGraph(nodes.shape[0], [(i['u'], i['v']) for _, i in edges.iterrows()], edges["length"])
+    
+    model.train()
+    for (u, v), positive_samples in trips["test"].items():
+        trips_input = [torch.LongTensor(trip).to(device) for trip in positive_samples]
+        positive_lengths = model(trips_input)
+        negative_samples = graph.yen_drop(positive_samples, u, v, 200)
+        trips_input = [torch.LongTensor(trip).to(device) for trip, _ in negative_samples]
+        negative_lengths = model(trips_input)
+        loss = bpr_loss_reverse(positive_lengths, negative_lengths)
+        print(loss)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-
-    m.eval()
-    with torch.no_grad():
-        result = m(x_idx, x_train)
-        print(lossfunc(result, y_train))
