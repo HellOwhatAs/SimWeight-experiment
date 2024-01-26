8869982370577c971f95a254a66c0322a32b0330
HellOwhatAs <xjq701229@outlook.com>
2024-01-26 20:50:01 +0800

    just one table

--- neg_sample.py
+++ neg_sample.py
@@ -1,14 +1,15 @@
+from typing import Literal
 from extract_data import Result, Trips
 import pickle
 import utils_rs
 from tqdm import tqdm
 
-def sampling_tosqlite(path: str, g: utils_rs.DiGraph, trips: Trips, k: int = 32, chunk_size: int = 4096) -> None:
-    for table, v_trips in trips.items():
-        uvs = list(v_trips.keys())
-        pos_samples = list(v_trips.values())
-        pbar = tqdm(total=len(uvs), desc=table)
-        g.par_path_sampling_tosqlite(uvs, pos_samples, k, chunk_size, path, table, False, pbar.update)
+def sampling_tosqlite(path: str, g: utils_rs.DiGraph, trips: Trips, table: Literal["train", "test", "valid"], k: int = 32, chunk_size: int = 4096) -> None:
+    v_trips = trips[table]
+    uvs = list(v_trips.keys())
+    pos_samples = list(v_trips.values())
+    pbar = tqdm(total=len(uvs), desc=table)
+    g.par_path_sampling_tosqlite(uvs, pos_samples, k, chunk_size, path, table, False, pbar.update)
 
 if __name__ == "__main__":
     # output file should be avaliable at https://www.kaggle.com/code/xjq701229/simweight-neg-sample
@@ -16,4 +17,4 @@ if __name__ == "__main__":
         tmp: Result = pickle.load(f)
         (nodes, edges, trips) = tmp
     g = utils_rs.DiGraph(nodes.shape[0], [(i['u'], i['v']) for _, i in edges.iterrows()], edges["length"])
-    sampling_tosqlite("beijing.db", g, trips)
+    sampling_tosqlite("beijing.db", g, trips, "valid")
