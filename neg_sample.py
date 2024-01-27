ebb2a1288cfb654d3b3113181e0efec836176939
HellOwhatAs <xjq701229@outlook.com>
2024-01-27 09:48:22 +0800

    load-able negative samples

--- neg_sample.py
+++ neg_sample.py
@@ -1,20 +1,34 @@
-from typing import Literal
+from typing import Literal, Optional
 from extract_data import Result, Trips
-import pickle
+import pickle, sys
 import utils_rs
 from tqdm import tqdm
 
-def sampling_tosqlite(path: str, g: utils_rs.DiGraph, trips: Trips, table: Literal["train", "test", "valid"], k: int = 32, chunk_size: int = 4096) -> None:
+Table = Literal["train", "test", "valid"]
+
+def sampling_tosqlite(path: str, g: utils_rs.DiGraph, trips: Trips, table: Table, k: int = 32, chunk_size: int = 4096) -> None:
     v_trips = trips[table]
     uvs = list(v_trips.keys())
     pos_samples = list(v_trips.values())
-    pbar = tqdm(total=len(uvs), desc=table)
+    pbar = tqdm(total=len(uvs), desc=table, file=sys.stdout)
     g.par_path_sampling_tosqlite(uvs, pos_samples, k, chunk_size, path, table, False, pbar.update)
 
+class SampleLoader:
+    def __init__(self, db_path: str, table: str) -> None:
+        self.sqlite = utils_rs.Sqlite(db_path, False)
+        self.default_table = table
+    
+    def get(self, u: int, v: int, table: Optional[Table] = None):
+        selected_table = self.default_table if table is None else table
+        return self.sqlite.get(selected_table, u, v)
+
+
 if __name__ == "__main__":
-    # output file should be avaliable at https://www.kaggle.com/code/xjq701229/simweight-neg-sample
+    # output file avaliable at https://www.kaggle.com/code/xjq701229/simweight-neg-sample/
     with open("./beijing.pkl", "rb") as f:
         tmp: Result = pickle.load(f)
         (nodes, edges, trips) = tmp
     g = utils_rs.DiGraph(nodes.shape[0], [(i['u'], i['v']) for _, i in edges.iterrows()], edges["length"])
+    sampling_tosqlite("beijing.db", g, trips, "train")
+    sampling_tosqlite("beijing.db", g, trips, "test")
     sampling_tosqlite("beijing.db", g, trips, "valid")
