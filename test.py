78507488633a13f2dd6f55e16d8fdac12ba7d652
HellOwhatAs <xjq701229@outlook.com>
2024-04-06 23:36:43 +0800

    commit

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ import vis_map
 import more_itertools
 from rower_model import Rower
 import torch
-import cmap
+import cmap, re
 import pandas as pd
+import numpy as np
 
 with open("./beijing.pkl", "rb") as f:
     tmp: Result = pickle.load(f)
@@ -32,14 +33,48 @@ def test_length_acc():
 def test_yen():
     map = vis_map.base_edge_map(nodes, edges,
         tiles= 'https://wprd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
-        attr='高德-常规图')
+        attr='高德-常规图', zoom_start=12)
     yen_trips = [path for path, _ in g.yen(10893, 7595, 200)]
-    vis_map.add_edges(map, nodes, edges, set(more_itertools.flatten(yen_trips)), color="#00FF00")
+    vis_map.add_edges(map, nodes, edges, set(more_itertools.flatten(yen_trips)), color="#CE4257")
     vis_map.add_nodes(map, nodes, {
         10893: "start",
         7595: "target"
     })
-    map.save("tmp0.html")
+    map.save("yen.html")
+
+def test_bidijstra():
+    map = vis_map.base_edge_map(nodes, edges,
+        tiles= 'https://wprd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
+        attr='高德-常规图', zoom_start=12)
+    tmp = g.bidirectional_dijkstra(10893, 7595, 200)
+    vis_map.add_edges(map, nodes, edges, more_itertools.flatten(tmp), color="#CE4257")
+    vis_map.add_nodes(map, nodes, {
+        10893: "start",
+        7595: "target"
+    })
+    map.save("bidijstra.html")
+
+def test_why_dynamic():
+    c: Dict[Tuple[int, int], Set[Tuple[int]]] = {}
+    tmp = edges[["u", "v"]].to_numpy().tolist()
+    for trip in tqdm(more_itertools.flatten(trips["train"].values())):
+        key = (tmp[trip[0]][0], tmp[trip[-1]][1])
+        if key in c: c[key].add(tuple(trip))
+        else: c[key] = {tuple(trip)}
+    max_val = max(c.items(), key=lambda x: len(x[1]))
+        
+    map = vis_map.base_edge_map(nodes, edges,
+        tiles= 'https://wprd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
+        attr='高德-常规图')
+    vis_map.add_trips(map, nodes, edges, max_val[1], color='#4AAD52')
+    # tmp_trips = [path for path, _ in g.yen(max_val[0][0], max_val[0][1], 2000)]
+    tmp_trips = g.bidirectional_dijkstra(max_val[0][0], max_val[0][1], len(max_val[1]))
+    vis_map.add_trips(map, nodes, edges, tmp_trips, color="#CE4257")
+    vis_map.add_nodes(map, nodes, {
+        max_val[0][0]: f"start: {max_val[0][0]}",
+        max_val[0][1]: f"target: {max_val[0][1]}"
+    })
+    map.save("why_dynamic.html")
 
 def test_map_case():
     c: Dict[Tuple[int, int], Set[Tuple[int]]] = {}
@@ -53,9 +88,9 @@ def test_map_case():
     map = vis_map.base_edge_map(nodes, edges,
         tiles= 'https://wprd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
         attr='高德-常规图')
-    vis_map.add_trips(map, nodes, edges, max_val[1])
-    yen_trips = [path for path, _ in g.yen(max_val[0][0], max_val[0][1], len(max_val[1]))]
-    vis_map.add_trips(map, nodes, edges, yen_trips, color="#00FF00")
+    vis_map.add_trips(map, nodes, edges, max_val[1], color='#4AAD52')
+    yen_trips = [path for path, _ in g.yen(max_val[0][0], max_val[0][1], 1)]
+    vis_map.add_trips(map, nodes, edges, yen_trips, color="#CE4257")
     vis_map.add_nodes(map, nodes, {
         max_val[0][0]: f"start: {max_val[0][0]}",
         max_val[0][1]: f"target: {max_val[0][1]}"
@@ -70,9 +105,9 @@ def test_map_case():
     map = vis_map.base_edge_map(nodes, edges,
         tiles= 'https://wprd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
         attr='高德-常规图')
-    vis_map.add_trips(map, nodes, edges, max_val[1])
-    yen_trips = [path for path, _ in g.yen(max_val[0][0], max_val[0][1], len(max_val[1]))]
-    vis_map.add_trips(map, nodes, edges, yen_trips, color="#00FF00")
+    vis_map.add_trips(map, nodes, edges, max_val[1], color='#4AAD52')
+    yen_trips = [path for path, _ in g.yen(max_val[0][0], max_val[0][1], 1)]
+    vis_map.add_trips(map, nodes, edges, yen_trips, color="#CE4257")
     vis_map.add_nodes(map, nodes, {
         max_val[0][0]: f"start: {max_val[0][0]}",
         max_val[0][1]: f"target: {max_val[0][1]}"
@@ -80,34 +115,28 @@ def test_map_case():
     map.save("tmp_after.html")
     g.weight = old_weight
 
-
-def test_neg_sample():
-    map = vis_map.base_edge_map(nodes, edges,
-        tiles= 'https://wprd01.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7',
-        attr='高德-常规图')
-    tmp = g.bidirectional_dijkstra(10893, 7595, 200)
-    vis_map.add_edges(map, nodes, edges, more_itertools.flatten(tmp))
-    vis_map.add_nodes(map, nodes, {
-        10893: "start",
-        7595: "target"
-    })
-    map.save("tmp0307.html")
-
 def test_g_weight_set():
     weight = g.weight
     g.weight = [0] * len(g.edges)
     assert all(i == 0 for i in g.weight)
     g.weight = weight
 
+def histogram_equalization(data: np.ndarray, bins: int = 1024):
+    hist, bins = np.histogram(data, bins=bins, range=(data.min(), data.max()))
+    cdf = hist.cumsum()
+    cdf_normalized = cdf / float(cdf.max())
+    equalized_data = np.interp(data, bins[:-1], cdf_normalized)
+    return equalized_data
+
 def test_delta_weight():
     model = Rower(edges)
     model.load_state_dict(torch.load('model_weights.pth'))
     old_weight = torch.tensor(g.weight)
     new_weight = torch.tensor(model.get_weight().flatten().cpu().numpy())
-    c_weight = (new_weight / old_weight / 2).min(torch.tensor(1))
-    cweight: List[float] = c_weight.tolist()
+    c_weight = histogram_equalization((new_weight / old_weight / 2).numpy())
+    cweight: List[float] = (c_weight / c_weight.max()).tolist()
 
-    cm = cmap.Colormap(['lime', (0.2, 'blue'), 'red', 'black'])
+    cm = cmap.Colormap('viridis_r')
     color = [cm(i).hex for i in cweight]
     m = vis_map.colored_edge_map(nodes, edges, color, zoom_start=12)
     m.save("delta_weight.html")
@@ -118,8 +147,17 @@ def test_weight_distribute():
     model.load_state_dict(torch.load('model_weights.pth'))
     old_weight = torch.tensor(g.weight)
     new_weight = torch.tensor(model.get_weight().flatten().cpu().numpy())
-    plt.plot(sorted((new_weight / old_weight)))
-    plt.show()
+
+    c_weight = np.array(sorted((new_weight / old_weight / 2)))
+    cweight = histogram_equalization(c_weight, 100000)
+
+    plt.figure(figsize=(12,6))
+    ax = plt.subplot(121)
+    ax.set_yscale('log')
+    plt.scatter(range(len(c_weight)), c_weight, alpha=0.05)
+    plt.subplot(122)
+    plt.scatter(range(len(cweight)), cweight, alpha=0.05)
+    plt.savefig("weight_distribute.png", dpi=600)
 
 def test_unlearned_edges():
     model = Rower(edges)
@@ -128,6 +166,6 @@ def test_unlearned_edges():
     tmp_edges = edges.copy(deep=True)
     tmp_edges['weight'] = new_weight
 
-    vis_map.base_edge_map(nodes, tmp_edges.loc[(tmp_edges['length'] - tmp_edges['weight']).abs() < 0.1], zoom_start=12).save("unlearned_edges.html")
+    vis_map.base_edge_map(nodes, tmp_edges.loc[(tmp_edges['length'] - tmp_edges['weight']).abs() < 0.1], zoom_start=12, color='black').save("unlearned_edges.html")
 
-test_weight_distribute()
+test_unlearned_edges()
